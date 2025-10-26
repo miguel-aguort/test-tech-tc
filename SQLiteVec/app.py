24e004748a43bd296bf949ae3a608d419d71f8d8
Ubuntu <miguel.aguort@gmail.com>
2025-10-26 19:01:16 +0000

    fix(sqlitevec): add azure compatibility

--- SQLiteVec/app.py
+++ SQLiteVec/app.py
@@ -1,23 +1,24 @@
-from langchain_community.embeddings.sentence_transformer import (
-    SentenceTransformerEmbeddings,
-)
+from langchain_community.embeddings.sentence_transformer import SentenceTransformerEmbeddings
 from langchain_community.vectorstores import SQLiteVec
 import sqlite3
+import sqlite_vec
 
-# Step 1: Create a connection to the SQLite database
-connection = sqlite3.connect("./data/tmp/vec.db")
+connection = sqlite3.connect(":memory:")
+# Issues working in azure: 
+connection.row_factory = sqlite3.Row
+connection.enable_load_extension(True)
+sqlite_vec.load(connection)
 
 embedding_function = SentenceTransformerEmbeddings(model_name="all-MiniLM-L6-v2")
+
 vector_store = SQLiteVec(
-    table="state_union", 
-    db_file="./data/tmp/vec.db", 
-    embedding=embedding_function, 
+    table="state_union",
+    db_file=":memory:",
+    embedding=embedding_function,
     connection=connection
 )
 
-vector_store.add_texts(texts=["Ketanji Brown Jackson is awesome", "foo", "bar"])
-connection.commit()
-
+vector_store.add_texts(["Ketanji Brown Jackson is awesome", "foo", "bar"])
 data = vector_store.similarity_search("Ketanji Brown Jackson", k=4)
 
-connection.close()
+print(data)
