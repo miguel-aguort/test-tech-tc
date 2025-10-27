c3813b2d112fc8de6743dea8eea1fe0b720a8791
Ubuntu <miguel.aguort@gmail.com>
2025-10-27 17:14:41 +0000

    feat(assistant): main app.py file updated with load file exist, init db and ingest data from multimodal-ocr

--- Assistant/app.py
+++ Assistant/app.py
@@ -2,8 +2,9 @@
 import logging
 import os
 from dotenv import load_dotenv
+import pandas as pd
 
-from utils.database import multimodal_ocr
+from utils.database import multimodal_ocr, init_db, load_data
 
 # 0- config
 load_dotenv()
@@ -12,10 +13,15 @@ LOG_LEVEL = os.getenv('LOG_LEVEL')
 level = getattr(logging, LOG_LEVEL.upper(), logging.INFO)
 _log = logging.getLogger(__name__)
 
-# 1- Generar parquet
-_log
+# 1- Generar parquet if not exists
 multimodal_ocr()
 
 # 2- Cargar parquet e ingestar datos en sqlite vec
+df = load_data()
+pdf_content = df["contents"].astype(str).tolist()
+
+vector_db = init_db()
+vector_db.add_texts(pdf_content)
+
 
 # 3-  
